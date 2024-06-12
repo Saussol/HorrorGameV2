072f7a0e43fb78ef1e8f92dc25896590bfb1612e
Saussol <60229019+Saussol@users.noreply.github.com>
2024-06-12 13:58:35 +0200

    push pour mitchado

---
+++ Assets/01_Scripts/SetUpGamemanag/QuestGestion.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Netcode;
+using UnityEngine;
+
+public class QuestGestion : NetworkBehaviour
+{
+    private NetworkVariable<int> m_Variable = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+
+    public override void OnNetworkSpawn()
+    {
+        m_Variable.OnValueChanged += (int previousValue, int newValue) =>
+        {
+            Debug.Log(OwnerClientId + " mon num " + m_Variable.Value);
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsOwner) return;
+
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            m_Variable.Value += 1;           
+        }
+
+
+    }
+}
