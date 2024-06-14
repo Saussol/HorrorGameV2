08cf0e11d61bc3e3654c88034b1df4fbf322768e
Saussol <60229019+Saussol@users.noreply.github.com>
2024-06-14 10:01:08 +0200

    lobby name

---
+++ Assets/01_Scripts/Lobby/LobbyPlayers.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LobbyPlayers : MonoBehaviour
+{
+    public static LobbyPlayers Instance;
+
+    public GameObject[] players; //0 == host    1 2 3 == client
+    public TextMeshProUGUI[] playersName;
+
+    public List<GameObject> playersObj = new List<GameObject>();
+
+    public void Awake()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i].SetActive(false);
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        // Récupérer tous les objets avec le tag "Player"
+        GameObject[] playerObjects = GameObject.FindGameObjectsWithTag("Player");
+
+        // Ajouter les objets récupérés dans le HashSet
+        foreach (GameObject playerObject in playerObjects)
+        {
+            if (!playersObj.Contains(playerObject))
+            {
+                playersObj.Add(playerObject);
+            }
+        }
+
+        for (int i = 0; i < playersObj.Count; i++)
+        {
+            players[i].SetActive(true);
+            playersName[i].text = playersObj[i].GetComponent<PlayerNameSync>().name;
+        }
+    }
+}
---
+++ Assets/01_Scripts/Lobby/PlayerNameSync.cs
@@ -0,0 +1,68 @@
+using Steamworks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
+using Unity.Collections;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerNameSync : NetworkBehaviour
+{
+    public NetworkVariable<FixedString32Bytes> displayName = new NetworkVariable<FixedString32Bytes>();
+    //private CSteamID steamID;
+    public TextMeshPro textName;
+    public string name;
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            //steamID = SteamUser.();
+            string steamName = SteamClient.Name;
+            name = SteamClient.Name;
+
+            // Mettre à jour le pseudonyme sur le serveur
+            UpdateDisplayNameServerRpc(steamName);
+        }
+        else
+        {
+            // Mettre à jour l'affichage du pseudonyme lorsque le joueur rejoint la partie
+            displayName.OnValueChanged += OnDisplayNameChanged;
+        }
+
+        // Mettre à jour l'affichage initial
+        OnDisplayNameChanged(default, displayName.Value);
+    }
+
+    [ServerRpc]
+    private void UpdateDisplayNameServerRpc(string newName, ServerRpcParams rpcParams = default)
+    {
+        displayName.Value = newName;
+        UpdateDisplayNameClientRpc(newName);
+    }
+
+    [ClientRpc]
+    private void UpdateDisplayNameClientRpc(string newName)
+    {
+        displayName.Value = newName;
+        textName.text = newName;
+    }
+
+    private void OnDisplayNameChanged(FixedString32Bytes oldName, FixedString32Bytes newName)
+    {
+        Debug.Log("Display Name Changed: " + newName);
+        textName.text = newName.ToString();
+    }
+
+    [ServerRpc]
+    public void ChangeDisplayNameServerRpc(string newName)
+    {
+        // Vérifier si le joueur a les droits nécessaires pour changer de pseudonyme (à implémenter)
+        // ...
+
+        // Mettre à jour le pseudonyme sur le serveur
+        displayName.Value = newName;
+        UpdateDisplayNameClientRpc(newName);
+    }
+}
