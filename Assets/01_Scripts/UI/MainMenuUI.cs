9cb7dd4761b699fe38011d798bc589f1cbd81286
Saussol <60229019+Saussol@users.noreply.github.com>
2024-06-25 15:49:53 +0200

    asd

--- Assets/01_Scripts/UI/MainMenuUI.cs
+++ Assets/01_Scripts/UI/MainMenuUI.cs
@@ -6,50 +6,63 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuUI : NetworkBehaviour
 {
-	[SerializeField] private GameObject hostPanel, startPanel, loadingPanel;
+    [SerializeField] private GameObject hostPanel, startPanel, loadingPanel;
 
     public void HostGame()
-	{
-		hostPanel.SetActive(false);
-		GameNetworkManager.Instance.StartHost(4);
-		startPanel.SetActive(true);
-	}
+    {
+        hostPanel.SetActive(false);
+        GameNetworkManager.Instance.StartHost(4);
+        startPanel.SetActive(true);
+    }
 
-	public void JoinGame()
-	{
-		hostPanel.SetActive(false);
-	}
+    public void JoinGame()
+    {
+        hostPanel.SetActive(false);
+    }
 
-	public void StartGame()
-	{
-		if (NetworkManager.Singleton.IsHost)
-		{
-			StartCoroutine(StartWithLoading());
-		}
-	}
+    public void StartGame()
+    {
+        if (NetworkManager.Singleton.IsHost)
+        {
+            StartCoroutine(StartWithLoading());
+        }
+    }
 
-	private IEnumerator StartWithLoading()
-	{
-		SetLoadingScreenClientRpc();
-		yield return new WaitForSeconds(.5f);
-		NetworkManager.Singleton.SceneManager.LoadScene("MultiScene 2", LoadSceneMode.Single);
-	}
+    private IEnumerator StartWithLoading()
+    {
+        SetLoadingScreenClientRpc();
+        yield return new WaitForSeconds(.5f);
+        NetworkManager.Singleton.SceneManager.LoadScene("MultiScene 2", LoadSceneMode.Single);
+    }
 
-	[ClientRpc]
-	private void SetLoadingScreenClientRpc()
-	{
-		hostPanel.SetActive(false);
-		startPanel.SetActive(false);
-		loadingPanel.SetActive(true);
-	}
+    [ClientRpc]
+    private void SetLoadingScreenClientRpc()
+    {
+        hostPanel.SetActive(false);
+        startPanel.SetActive(false);
+        loadingPanel.SetActive(true);
+    }
 
-	public void exitGame()
-	{
-
-		/*if ()
-		{
-
-		}*/
+    public void ExitGame()
+    {
+        if (NetworkManager.Singleton.IsHost)
+        {
+            ReturnToLobby();
+        }
         Application.Quit();
     }
+
+    private void ReturnToLobby()
+    {
+        StartCoroutine(ReturnWithLoading());
+    }
+
+    private IEnumerator ReturnWithLoading()
+    {
+        SetLoadingScreenClientRpc();
+        yield return new WaitForSeconds(.5f);
+        NetworkManager.Singleton.SceneManager.LoadScene("LobbyScene", LoadSceneMode.Single);
+    }
+
+    
 }
